5889136ff9e99726cc1dc6f22f0a30b76f41860a
geo653rge <georgekimanikiringa@gmail.com>
2023-11-15 13:01:06 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css'
+import React, { useState }
+ from 'react';
+
+
+
+function App(){
+  const[name,Setname]=useState("");
+  const[number,Setnumber]=useState("");
+  const[email,Setemail]=useState("");
+  const[dateofbirth,Setdateofbirth]=useState("");
+  const[county,Setcounty]=useState("");
+  const[age,Setage]=useState("");
+  const[gender,Setgender]=useState("");
+  const[status,Setstatus]=useState("");
+return(
+
+<div className="App">
+
+<h1>"My name is George Kimani K"</h1>
+<form action='kimani' className="kimani">
+
+<input placeholder='name'type = "text" value={name} onChange={(event)=>Setname(event.target.value)}/>
+<input  placeholder='number' type = "number" value={number} onChange={(event)=>Setnumber(event.target.value)}/>
+<input  placeholder='email' type = "text" value={Text} onChange={(event)=>Setemail(event.target.value)}   />
+<input  placeholder='dateofbirth' type = "date"value={Date} onChange={(event)=>Setdateofbirth(event.target.value)}/>
+<input  placeholder='county' type = "number"value={county} onChange={(event)=>Setcounty(event.target.value)}/>
+<input  placeholder='age' type = "number" onChange={(event)=>Setage(event.target.value)}/>
+<input  placeholder='gender' type = "text" onChange={(event)=>Setgender(event.target.value)}/>
+<input  placeholder='Status' type = "text" onChange={(event)=>Setstatus(event.target.value)}/>
+
+  
+
+
+</form>
+<div>
+  <h1>Name: {name}</h1>
+  <h1>Number: {number}</h1>
+  <h1>Email: {email}</h1>
+  <h1>D.O.B: {dateofbirth}</h1>
+  <h1>Place of Origin: {county}</h1>
+  <h1>Age: {age}</h1>
+  <h1>Gender: {gender}</h1>
+  <h1>Status: {status}</h1>
+</div>
+
+</div>
+
+
+)
 }
 
-export default App;
+export default App ;
